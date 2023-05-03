d0b4e8597b1243d4eeeea232e5b8d14fa9ded6a2
flogus <fg@motionery.com>
2023-05-03 09:57:11 +0200

    fix: make some corrections for a working project

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,40 @@
 import React from 'react'
 
-import { ExampleComponent } from 'flogmodal'
+import { FlogModalComponent } from 'flogmodal'
 import 'flogmodal/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div style={{ display: 'flex' }}>
+      <FlogModalComponent
+        linkType='link'
+        buttonLabel='Open a great modal'
+        titleLabel='Great modal'
+        hasDefaultButton='true'
+        content='Content'
+        linkTitle='Open the modal with a link'
+      />
+      <FlogModalComponent
+        linkType='button'
+        buttonLabel='Modal with no title'
+        content='You can custon the content of the modal'
+        linkTitle='Open the modal with a button'
+      />
+      <FlogModalComponent
+        linkType='image'
+        buttonLabel='Custom buttons'
+        titleLabel='Modal with custom buttons'
+        hasDefaultButton='true'
+        cancelLabel='Annuler'
+        saveLabel='Envoyer'
+        imageSrc='https://cdn.cdnlogo.com/logos/r/63/react.svg'
+        imageHeight='100px'
+        imageWidth='100px'
+        imageAlt='React Logo'
+        linkTitle='Open the modal with a image'
+      />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,125 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const FlogModalComponent = ({
+  content,
+  buttonLabel,
+  titleLabel,
+  hasDefaultButton,
+  cancelLabel,
+  saveLabel,
+  linkType,
+  imageSrc,
+  imageHeight,
+  imageWidth,
+  imageAlt,
+  linkTitle
+}) => {
+  const [isOpen, setOpen] = useState(false)
+
+  const openModal = () => {
+    if (isOpen) {
+      setOpen(false)
+    } else {
+      setOpen(true)
+    }
+  }
+  const closeButton = () => {
+    setOpen(false)
+  }
+  const closeWidow = (e) => {
+    if (e.target === e.currentTarget) {
+      setOpen(false)
+    }
+  }
+
+  const DefaultButton = ({ DefaultButton }) => {
+    if (hasDefaultButton) {
+      return (
+        <div className={styles.defaultButtonsContainer}>
+          <button onClick={closeButton}>
+            {cancelLabel ? cancelLabel : 'Cancel'}
+          </button>
+          <button className={styles.primary}>
+            {saveLabel ? saveLabel : 'Save'}
+          </button>
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+
+  const Title = ({ titleLabel }) => {
+    return <h1>{titleLabel}</h1>
+  }
+
+  useEffect(() => {
+    function handleEscapeKey (event) {
+      if (event.code === 'Escape') {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener('keydown', handleEscapeKey)
+    return () => document.removeEventListener('keydown', handleEscapeKey)
+  }, [])
+
+  const TypeOfLink = ({ linkType }) => {
+    switch (linkType) {
+      case 'link':
+        return (
+          <a href='#' title={linkTitle} onClick={openModal}>
+            {buttonLabel}
+          </a>
+        )
+      case 'image':
+        return (
+          <a href='#' title={linkTitle} height={imageHeight} width={imageWidth}>
+            <img
+              src={imageSrc}
+              onClick={openModal}
+              height={imageHeight}
+              width={imageWidth}
+              alt={imageAlt}
+            />
+          </a>
+        )
+      default:
+        return (
+          <button title={linkTitle} onClick={openModal}>
+            {buttonLabel}
+          </button>
+        )
+    }
+  }
+
+  const theModal = (
+    <div className={styles.modalbuttonGlobal}>
+      <TypeOfLink
+        linkType={linkType}
+        imageSrc={imageSrc}
+        imageHeight={imageHeight}
+        imageWidth={imageWidth}
+        imageAlt={imageAlt}
+        linkTitle={linkTitle}
+      />
+      <div
+        className={isOpen ? styles.modal : styles.modalClose}
+        onClick={(e) => closeWidow(e)}
+      >
+        <div className={styles.modalcontent}>
+          <div className={styles.crossButtonContainer}>
+            <button className={styles.crossButton} onClick={closeButton}>
+              x
+            </button>
+          </div>
+          {titleLabel ? <Title titleLabel={titleLabel} /> : null}
+          <div className={styles.mainContent}>{content}</div>
+          <DefaultButton />
+        </div>
+      </div>
+    </div>
+  )
+  return theModal
 }
